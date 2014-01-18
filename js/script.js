a6156f11cbb0a9fd07373332abc1cf70f018b204
Krzysztof Golinski <kgolinski@pangenerator.com>
2014-01-18 21:52:36 +0100

    front ready with pagination and stuff

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
+var canvasBg, ctxBg;
 $(function(){
-	
-	canvas = document.getElementById("canvas");
-	ctx = canvas.getContext('2d'); 
+	canvasBg = document.getElementById("canvas-bg");
+	ctxBg = canvasBg.getContext('2d'); 
 	clear();
 	window.addEventListener('resize', resize);
     resize();
 });
 function drawBg(width, color, lineColor, angle){
-  ctx.beginPath();
-	ctx.moveTo(0,0);
-	ctx.lineTo(canvas.height*Math.sin(42.5*Math.PI/180),canvas.height); 
-	ctx.lineWidth=0.5;
-	ctx.strokeStyle=lineColor;
-	ctx.stroke();
+    ctxBg.beginPath();
+	ctxBg.moveTo(0,0);
+	ctxBg.lineTo(canvasBg.height*Math.sin(42.5*Math.PI/180),canvasBg.height); 
+	ctxBg.lineWidth=0.5;
+	ctxBg.strokeStyle=lineColor;
+	ctxBg.stroke();
 }
 function clear() {
-	ctx.clearRect(0, 0, canvas.width,  canvas.height);
+	ctxBg.clearRect(0, 0, canvasBg.width,  canvasBg.height);
 }
 function resize() {
-	document.getElementById("canvas").setAttribute('width', 1024);
-	document.getElementById("canvas").setAttribute('height', Math.max(768,$(document).height()));
+	document.getElementById("canvas-bg").setAttribute('width', 1024);
+	document.getElementById("canvas-bg").setAttribute('height', Math.max(768,$(document).height()));
 	drawBg(278, "rgba(255,192,0,1.0)", "rgba(0,0,0,0.3)",30);
 }
