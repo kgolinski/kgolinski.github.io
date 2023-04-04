93cc9eca8a3f858d0b08957998dc3758e51d5658
Krzysztof Goliński <krzysztof@golinski.org>
2023-04-04 10:09:28 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,12 @@ let canvasBg, ctxBg;
 document.addEventListener("DOMContentLoaded", (e) => {
 	canvasBg = document.getElementById("canvas-bg");
 	ctxBg = canvasBg.getContext('2d');
-	clear();
 	window.addEventListener('resize', resize);
 	resize();
 });
 
 const drawBg = (width, color, lineColor, angle) => {
+	clear();
 	ctxBg.beginPath();
 	ctxBg.moveTo(0, 0);
 	ctxBg.lineTo(canvasBg.height * Math.sin(42.5 * Math.PI / 180), canvasBg.height);
@@ -23,6 +23,6 @@ const clear = () => {
 
 const resize = () => {
 	document.getElementById("canvas-bg").setAttribute('width', 1024);
-	document.getElementById("canvas-bg").setAttribute('height', Math.max(768, document.scrollHeight));
+	document.getElementById("canvas-bg").setAttribute('height', Math.max(768, document.documentElement.scrollHeight));
 	drawBg(278, "rgba(255,192,0,1.0)", "rgba(0,0,0,0.3)", 30);
 }
