8b0fc6818963aa154cda98124a41311e22651a1e
Krzysztof Goliński <krzysztof@golinski.org>
2023-04-04 09:51:06 +0200

    jquery removed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,28 @@
-var canvasBg, ctxBg;
-$(function(){
+let canvasBg, ctxBg;
+
+document.addEventListener("DOMContentLoaded", function (e) {
 	canvasBg = document.getElementById("canvas-bg");
-	ctxBg = canvasBg.getContext('2d'); 
+	ctxBg = canvasBg.getContext('2d');
 	clear();
 	window.addEventListener('resize', resize);
-    resize();
-    $(".lineno").first().addClass('lineno-first');
-    $(".lineno").last().addClass('lineno-last');
+	resize();
 });
-function drawBg(width, color, lineColor, angle){
-    ctxBg.beginPath();
-	ctxBg.moveTo(0,0);
-	ctxBg.lineTo(canvasBg.height*Math.sin(42.5*Math.PI/180),canvasBg.height); 
-	ctxBg.lineWidth=0.5;
-	ctxBg.strokeStyle=lineColor;
+
+const drawBg = (width, color, lineColor, angle) => {
+	ctxBg.beginPath();
+	ctxBg.moveTo(0, 0);
+	ctxBg.lineTo(canvasBg.height * Math.sin(42.5 * Math.PI / 180), canvasBg.height);
+	ctxBg.lineWidth = 0.5;
+	ctxBg.strokeStyle = lineColor;
 	ctxBg.stroke();
 }
-function clear() {
-	ctxBg.clearRect(0, 0, canvasBg.width,  canvasBg.height);
+
+const clear = () => {
+	ctxBg.clearRect(0, 0, canvasBg.width, canvasBg.height);
 }
-function resize() {
+
+const resize = () => {
 	document.getElementById("canvas-bg").setAttribute('width', 1024);
-	document.getElementById("canvas-bg").setAttribute('height', Math.max(768,$(document).height()));
-	drawBg(278, "rgba(255,192,0,1.0)", "rgba(0,0,0,0.3)",30);
+	document.getElementById("canvas-bg").setAttribute('height', Math.max(768, $(document).height()));
+	drawBg(278, "rgba(255,192,0,1.0)", "rgba(0,0,0,0.3)", 30);
 }
